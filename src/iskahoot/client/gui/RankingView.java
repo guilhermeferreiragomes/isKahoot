5e91bdb0eb9648e5c8943346b651ce5f358fff42
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2025-11-10 16:00:30 +0000

    commit

--- src/iskahoot/client/gui/RankingView.java
+++ src/iskahoot/client/gui/RankingView.java
@@ -4,23 +4,39 @@ import java.awt.BorderLayout;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class RankingView extends JPanel {
 	
 	private View view;
-	
+	private JLabel title;
+	private JTextArea rankings;
+
 	public RankingView(View view) {
 		this.view = view;
-	
-		setLayout(new BorderLayout());
+
+		this.setLayout(new BorderLayout(10, 10));
+		this.setBorder(new EmptyBorder(10, 200, 10, 200));
+
+		//Titulo em cima
+		this.title = new JLabel("CLASSIFICAÇÃO", SwingConstants.CENTER);
+		add(title, BorderLayout.NORTH);
+
+		//Rankings
+		this.rankings = new JTextArea();
+		rankings.setEditable(false); // Se não dá para escrever
+		add(rankings, BorderLayout.CENTER);
+
+		//Temporario
+		rankings.setText("RESULTADOS DA RONDA: \n\n");
+		rankings.append("Equipa Gui - 100 pontos\n");
+		rankings.append("Equipa Francisco - 100 pontos\n");
 		
-		JLabel tempLabel = new JLabel("Aqui vai aparecer o Placar/Ranking");
-        tempLabel.setHorizontalAlignment(SwingConstants.CENTER);
-        
-        add(tempLabel, BorderLayout.CENTER);
-		
-		
+		JScrollPane scrollPane = new JScrollPane(rankings); //caso seja muito comprido
+		add(scrollPane, BorderLayout.CENTER);
 	}
 
 }
