c6cd486f5692a41caf15d0cf19634710e3dd0e2c
Guilherme Ferreira Gomes <gffgs@iscte-iul.pt>
2025-11-23 19:46:00 +0000

    commit

--- src/iskahoot/server/GameServer.java
+++ src/iskahoot/server/GameServer.java
@@ -1,32 +1,47 @@
 package iskahoot.server;
 
-import iskahoot.game.Question;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import iskahoot.game.Quiz;
 
 public class GameServer {
+    public static final int PORT = 8080;
 
     public static void main(String[] args) {
-        
         System.out.println("--- A testar o QuizLoader ---");
-
         Quiz quiz = QuizLoader.loadQuizFromFile("data/quizzes.json");
         
         if (quiz != null && quiz.getQuestions() != null) {
-            
             System.out.println("Quiz '" + quiz.getName() + "' carregado.");
             System.out.println("Total de perguntas: " + quiz.getQuestions().size());
-            System.out.println("--------------------------------------");
-
-            for (Question q : quiz.getQuestions()) {
-                System.out.println("Pergunta: " + q.getQuestion());
-                System.out.println("  Opções: " + q.getOptions());
-                System.out.println("  Correta (índice): " + q.getCorrect());
-                System.out.println("  Pontos: " + q.getPoints());
-                System.out.println();
+            
+            try {
+                System.out.println("🚀 A iniciar o servidor...");
+                new GameServer().startServing();
+            } catch (IOException e) {
+                System.err.println("❌ Erro a iniciar o servidor: " + e.getMessage());
             }
             
         } else {
             System.err.println("ERRO: O QuizLoader falhou.");
+        }
+    }
+
+    public void startServing() throws IOException {
+        ServerSocket ss = new ServerSocket(PORT);
+        System.out.println("✅ Servidor pronto na porta " + PORT);
+        
+        try {
+            while (true) {
+                Socket clientSocket = ss.accept();
+                System.out.println("🔌 Nova conexão recebida!");
+                
+                PlayerThread playerThread = new PlayerThread(clientSocket);
+                playerThread.start();
+            }
+        } finally {
+            ss.close();
         }
     }
 }
--- src/iskahoot/server/PlayerThread.java
+++ src/iskahoot/server/PlayerThread.java
@@ -1,5 +1,53 @@
 package iskahoot.server;
 
-public class PlayerThread {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class PlayerThread extends Thread {
+
+    private Socket clientSocket;
+    private BufferedReader in; //Ler o que o cliente comunica
+    private PrintWriter out; //Dizer ao cliente o que o server comunica
+
+    public PlayerThread(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            connectToServer();
+           System.out.println("PlayerThread #" +  Thread.currentThread().threadId() + " a conectar ao servidor");
+           listenToClient();
+        } catch (Exception e) {
+            System.err.println("Erro de conexão " + e.getMessage());
+        }
+    }
+
+    private void connectToServer() throws IOException {
+        this.in = new BufferedReader(new InputStreamReader(
+            clientSocket.getInputStream()
+        ));
+        this.out = new PrintWriter(new BufferedWriter(
+            new OutputStreamWriter(clientSocket.getOutputStream())), true
+        );
+    }
+
+    private void listenToClient() throws IOException {
+        String message;
+        while ((message = in.readLine()) != null) {
+            System.out.println("Recebido: " + message);
+            if (message.equals("FIM")) {
+                break; 
+            }
+        }
+        System.out.println("Cliente desconectou");
+    }
+
 
 }
